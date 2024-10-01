7b2e6c763a006375bae1dc422eca7df4795b9662
GISEAkriarBU <thanapoon.keaw@bumail.net>
2024-10-01 11:05:47 +0700

    crocrodile

--- Assets/Scripts/lab/Crocodile.cs
+++ Assets/Scripts/lab/Crocodile.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class Crocodile : Enemy
 {
-    private float attackRangr;
-    private Player player;
-
+    [SerializeField] private float attackRange;
+    [SerializeField] private Player player;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawn;
+    [SerializeField] private float bulletWaitTime;
+    [SerializeField] private float bulletTimer;
     public void Start()
     {
         Init(100);
         Debug.Log($"{Health}");
+      
+    }
+    private void Update()
+    {
+        bulletTimer = Time.deltaTime;
         Behaviour();
+        if (bulletTimer < 0) 
+        {
+            bulletTimer = bulletWaitTime;
+        }
     }
 
     public override void Behaviour()
     {
-        Debug.Log("Croccodile ");
+        Vector2 direction = player.transform.position - transform.position;
+        float distance = direction.magnitude;
+        if ( distance < attackRange ) { Debug.Log("shoot"); }
     }
 
+    private void shoot () 
+    { 
+        if (bulletTimer < 0) 
+        { 
+           Instantiate(bullet,bulletSpawn.position,Quaternion.identity);
+        }
+     
+    }
 }
