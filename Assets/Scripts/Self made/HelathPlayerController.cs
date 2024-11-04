2f0583a4ce350c3c9a2cd12a9b901832c7037fe3
GISEAkriarBU <thanapoon.keaw@bumail.net>
2024-11-04 19:12:29 +0700

    added detail on commented
    intelisence เกิดปัญหาตวรจไม่พบ UnityEngine.UI

--- Assets/Scripts/Self made/HelathPlayerController.cs
+++ Assets/Scripts/Self made/HelathPlayerController.cs
@@ -1,36 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI;//intelisence เกิดปัญหาตวรจไม่พบ UnityEngine.UI
+
 
 public class HelathPlayerController : MonoBehaviour
 {
     private float _maxHealth = 100;
     private float _currentHealth;
-    [SerializeField] private Image _healthBarFill;
-    //private GameController ประกาศใช้ไม่ได้ 
-
+    [SerializeField] private Image _healthBarFill;//intelisence เกิดปัญหาตวรจไม่พบ UnityEngine.UI
+    [SerializeField] private float _damageAmount;
+    [SerializeField] private Character gameController;
+ 
     private void Awake()
     {
         _maxHealth = _currentHealth;
+       
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) // เช็คการtake ดาเมจ
     {
         if (collision.CompareTag("EnemyObject"))
         {
-            TakeDamage(10);
+            TakeDamage(_damageAmount);
         }
     }
-    private void TakeDamage(float amount)
+    private void TakeDamage(float amount) //มาลองรับดาเมจ
     {
         _currentHealth -= amount;
         _currentHealth = Mathf.Clamp(_currentHealth, 0, _maxHealth);
-        if (_currentHealth <= 0) { }
+        if (_currentHealth <= 0) { gameController.Isdead(); }
     }
 
-    private void UpdateHealthBar()
+    private void UpdateHealthBar() // ไว้เรองรับการเปลี่ยนแปลงของค่าเลือด 
     {
         _healthBarFill.fillAmount = _currentHealth / _maxHealth;
-    }
 
     }
+
+}
